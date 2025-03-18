53d7630658ad35efb152fc8f48d4602d87fb8b2b
Surihix <neweramysterymailer@gmail.com>
2025-03-18 14:38:21 +0530

    fixed typo for finished conversion message

--- WDBXlsxTool/XIII2LR/Conversion/ConversionMain.cs
+++ WDBXlsxTool/XIII2LR/Conversion/ConversionMain.cs
@@ -33,7 +33,7 @@
 
             Console.WriteLine("");
             Console.WriteLine("");
-            Console.WriteLine("Finished building wdb file for extracted json data");
+            Console.WriteLine("Finished building wdb file for extracted xlsx data");
         }
     }
 }
